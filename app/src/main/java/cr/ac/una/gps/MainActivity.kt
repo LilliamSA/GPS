a3a913f6948b31214414b98f78dd06880b53906d
JonathanPA12 <jonathan.pineda.aleman@est.una.ac.cr>
2023-03-27 20:19:03 -0600

    se configura la nevagcion basica del menu standar

--- app/src/main/java/cr/ac/una/gps/MainActivity.kt
+++ app/src/main/java/cr/ac/una/gps/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 
 
@@ -26,5 +27,13 @@ class MainActivity : AppCompatActivity() {
         )
         drawerLayout.addDrawerListener(toogle)
         toogle.syncState()
+    }
+
+    override fun onBackPressed() {
+    if(drawerLayout.isDrawerOpen(GravityCompat.START))
+        drawerLayout.closeDrawer(GravityCompat.START)
+
+        else
+            super.onBackPressed()
     }
 }
