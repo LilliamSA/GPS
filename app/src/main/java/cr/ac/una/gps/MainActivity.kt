85b784ec7a57e7549b8fa27af6fc4c2540346ec4
JonathanPA12 <jonathan.pineda.aleman@est.una.ac.cr>
2023-03-27 19:54:02 -0600

    Se asocia el drawelayout a la vista main

--- app/src/main/java/cr/ac/una/gps/MainActivity.kt
+++ app/src/main/java/cr/ac/una/gps/MainActivity.kt
@@ -3,13 +3,17 @@ package cr.ac.una.gps
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+        drawerLayout = findViewById(R.id.drawer_layout)
     }
 }
