55d0aafd78fb32176199d7093effe6faf24d06ca
JonathanPA12 <jonathan.pineda.aleman@est.una.ac.cr>
2023-04-13 12:29:10 -0600

    arreglo del collasing tool bar

--- app/src/main/java/cr/ac/una/gps/MainActivity.kt
+++ app/src/main/java/cr/ac/una/gps/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
-import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.navigation.NavigationView
 
 
