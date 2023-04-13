5d5158f8553e503fc3190cc34b0b6d8fa629d0e0
JonathanPA12 <jonathan.pineda.aleman@est.una.ac.cr>
2023-04-13 12:29:46 -0600

    estilos de mapas para mostrar

--- app/src/main/java/cr/ac/una/gps/MapsFragment.kt
+++ app/src/main/java/cr/ac/una/gps/MapsFragment.kt
@@ -29,6 +29,8 @@ class MapsFragment : Fragment() {
         val sydney = LatLng(-34.0, 151.0)
         googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        //googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE)
+
     }
 
     override fun onCreateView(
