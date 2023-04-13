5f8087ad48ef1bec1452250b6eeccfc3e406188e
JonathanPA12 <jonathan.pineda.aleman@est.una.ac.cr>
2023-04-13 12:53:34 -0600

    se agrega activity acercade

---
+++ app/src/main/java/cr/ac/una/gps/actitityAcercade.kt
@@ -0,0 +1,11 @@
+package cr.ac.una.gps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class actitityAcercade : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.actitity_acercade)
+    }
+}
