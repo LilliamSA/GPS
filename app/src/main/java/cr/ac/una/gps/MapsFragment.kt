41ac2cce83f0919f3a9d6e03dfa38f1b8d70c8af
Lilliam Solis <lilliam.solis.avila@est.una.ac.cr>
2023-04-16 17:25:37 -0600

    botones para acercar y alejar el mapa

--- app/src/main/java/cr/ac/una/gps/MapsFragment.kt
+++ app/src/main/java/cr/ac/una/gps/MapsFragment.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.content.Context
+import android.content.SharedPreferences
+import android.widget.Button
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -16,34 +19,69 @@ import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsFragment : Fragment() {
 
-    private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-        //googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE)
-
-    }
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
+        val view = inflater.inflate(R.layout.fragment_maps, container, false)
+
+        // Crear instancia de SharedPreferences
+        sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
+
+        return view
+    }
+    private val callback = OnMapReadyCallback { googleMap ->
+        // Recuperar el valor guardado en SharedPreferences
+        val textoMarcador = sharedPreferences.getString("textoMarcador", "")
+
+        // Crear un marcador en el mapa con el texto recuperado
+        val latLng = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(latLng).title(textoMarcador))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+        //
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 18f))
+
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
         super.onViewCreated(view, savedInstanceState)
+
+        // Obtén una referencia al botón de zoom in
+        val btnZoomIn = view.findViewById<Button>(R.id.btnZoomIn)
+
+        // Agrega un listener al botón de zoom in
+        btnZoomIn.setOnClickListener {
+            // Obtén una referencia al mapa
+            val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+            mapFragment?.getMapAsync { googleMap ->
+                // Incrementa el nivel de zoom
+                val currentZoom = googleMap.cameraPosition.zoom
+                googleMap.moveCamera(CameraUpdateFactory.zoomTo(currentZoom + 1))
+            }
+        }
+
+        // Obtén una referencia al botón de zoom out
+        val btnZoomOut = view.findViewById<Button>(R.id.btnZoomOut)
+
+        // Agrega un listener al botón de zoom out
+        btnZoomOut.setOnClickListener {
+            // Obtén una referencia al mapa
+            val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+            mapFragment?.getMapAsync { googleMap ->
+                // Decrementa el nivel de zoom
+                val currentZoom = googleMap.cameraPosition.zoom
+                googleMap.moveCamera(CameraUpdateFactory.zoomTo(currentZoom - 1))
+            }
+        }
+
+        // Obtener el SupportMapFragment y notificar cuando el mapa esté listo para ser usado.
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+
     }
 }
