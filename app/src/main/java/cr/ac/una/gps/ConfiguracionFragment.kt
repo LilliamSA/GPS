47fa394418570abfaa361ef9f744415337f89592
Lilliam Solis <lilliam.solis.avila@est.una.ac.cr>
2023-04-16 16:36:48 -0600

    parte de configuración funcional

--- app/src/main/java/cr/ac/una/gps/ConfiguracionFragment.kt
+++ app/src/main/java/cr/ac/una/gps/ConfiguracionFragment.kt
@@ -1,59 +1,61 @@
 package cr.ac.una.gps
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.content.Context
+import android.widget.Toast
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ConfiguracionFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ConfiguracionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var editMarcador: EditText
+    private lateinit var botonGuardar: Button
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_configuracion, container, false)
+        val view = inflater.inflate(cr.ac.una.gps.R.layout.fragment_configuracion, container, false)
+
+        // Obtener las referencias a los elementos de la interfaz
+        editMarcador = view.findViewById(R.id.textmarcador)
+        botonGuardar = view.findViewById(R.id.btnguardar)
+
+        // Asignar un listener al botón
+        botonGuardar.setOnClickListener {
+            guardarMarcador()
+        }
+
+        return view
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Cargar el texto guardado en SharedPreferences
+        val sharedPreferences = activity?.getPreferences(Context.MODE_PRIVATE)
+        val textoMarcador = sharedPreferences?.getString("textoMarcador", "")
+
+        // Establecer el texto en el EditText
+        editMarcador.setText(textoMarcador)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment configuracion.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ConfiguracionFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun guardarMarcador() {
+        val textoMarcador = editMarcador.text.toString()
+
+        // Guardar el texto del marcador en SharedPreferences
+        val sharedPreferences = activity?.getPreferences(Context.MODE_PRIVATE)
+        val editor = sharedPreferences?.edit()
+        editor?.putString("textoMarcador", textoMarcador)
+        editor?.apply()
+
+        // Mostrar un mensaje de confirmación
+        Toast.makeText(activity, "Marcador guardado correctamente", Toast.LENGTH_SHORT).show()
     }
+
+
 }
