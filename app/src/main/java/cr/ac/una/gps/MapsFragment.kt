dff00f1425cb0212f9fae056890d0c719dd425b1
Lilliam Solis <lilliam.solis.avila@est.una.ac.cr>
2023-04-16 19:42:58 -0600

    ubicación actual y permisos

--- app/src/main/java/cr/ac/una/gps/MapsFragment.kt
+++ app/src/main/java/cr/ac/una/gps/MapsFragment.kt
@@ -1,5 +1,7 @@
 package cr.ac.una.gps
 
+import android.Manifest
+import android.app.AlertDialog
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
@@ -7,9 +9,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.location.Location
+import android.media.audiofx.BassBoost
+import android.net.Uri
 import android.widget.Button
-
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -17,9 +27,14 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
+
+
+
 class MapsFragment : Fragment() {
 
     private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var maps: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,19 +47,6 @@ class MapsFragment : Fragment() {
         sharedPreferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
 
         return view
-    }
-    private val callback = OnMapReadyCallback { googleMap ->
-        // Recuperar el valor guardado en SharedPreferences
-        val textoMarcador = sharedPreferences.getString("textoMarcador", "")
-
-        // Crear un marcador en el mapa con el texto recuperado
-        val latLng = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(latLng).title(textoMarcador))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng))
-        //
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 18f))
-
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -81,7 +83,62 @@ class MapsFragment : Fragment() {
 
         // Obtener el SupportMapFragment y notificar cuando el mapa esté listo para ser usado.
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        mapFragment?.getMapAsync(callback)
+        mapFragment?.getMapAsync{
+            maps = it
+            getLocation()
+        }
 
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+    }
+
+    private fun getLocation() {
+        val textoMarcador = sharedPreferences.getString("textoMarcador", "")
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Pedir permisos de ubicación
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), 1)
+
+        } else {
+            // Obtener la ubicación actual y mostrarla en el mapa
+            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+                if (maps != null) {
+                    // Ubicación obtenida con éxito
+                    if (location != null) {
+                        val currentLatLng = LatLng(location.latitude, location.longitude)
+                        addMarkerToMap(currentLatLng, textoMarcador)
+                        maps.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
+                    }
+                }
+            }
+        }
+    }
+
+    private fun addMarkerToMap(latLng: LatLng, title: String?) {
+        maps.addMarker(MarkerOptions().position(latLng).title(title))
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == 1) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.checkSelfPermission(
+                        requireContext(),
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        requireContext(),
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) == PackageManager.PERMISSION_GRANTED
+                ) {
+
+                    getLocation()
+                }
+            } else {
+                Toast.makeText(requireContext(), "Permiso denegado", Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
     }
 }
